dae74fd2ff2432e21052d01f8c0ff95c3b996585
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-06-14 16:42:56 +0000

    Add new features

--- src/source.py
+++ src/source.py
@@ -15,3 +15,19 @@ def get_user_repositories(user):
     response = urllib.request.urlopen(request)
     data = json.loads(response.read().decode())
     return [repo['name'] for repo in data]
+
+# Get a user profile
+def get_user(user):
+    url = 'https://api.github.com/users/' + user 
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    data = json.loads(response.read().decode())
+    return data
+
+# Get a organizations profile
+def get_org(org):
+    url = 'https://api.github.com/orgs/' + org 
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    data = json.loads(response.read().decode())
+    return data
